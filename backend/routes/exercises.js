5801f04b8b79fcc2f81637238f40ee7b7d11a5fc
Steve Archuleta <stevearchuleta@me.com>
2020-03-07 02:37:38 -0800

    add a /:id get endroute to the exercise routes; add a /:id delete endroute to the exercise routes; add a /update post endroute to the exercise routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let User = require('../models/exercise.model');
+let Exercise = require('../models/exercise.model');
 
 //handles http get request at localhost:5000/exercises/
 router.route('/').get((req, res) => {
@@ -26,5 +26,38 @@ router.route('/add').post((req, res) => {
       .then(() => res.json('Exercise Added!'))
       .catch(err => res.status(400).json('Error: ' + err));
    });
+
+   //handles http get request at localhost:5000/exercises/<INSERT MONGODB OBJECT ID HERE>
+   router.route('/:id').get((req, res) => {
+      Exercise.findById(req.params.id)
+         .then(exercise => res.json(exercise))
+         .catch(err => res.status(400).json('Error: ' + err));
+      });
+
+   //handles http delete request at localhost:5000/exercises/<INSERT MONGODB OBJECT ID HERE>
+   router.route('/:id').delete((req, res) => {
+   Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Exercise deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+   });
+
+
+//handles http post request at localhost:5000/exercises//<INSERT MONGODB OBJECT ID HERE> by first finding the desired exercise by passing the parameter directly from the url, and then updating any of the exercise object's properties
+router.route('/update/:id').post((req, res) => {
+   Exercise.findById(req.params.id)
+      .then(exercise => {
+         exercise.username = req.body.username;
+         exercise.description = req.body.description;
+         exercise.duration = Number(req.body.duration);
+         exercise.date = Date.parse(req.body.date);
+         
+         exercise.save()
+            .then(() => res.json('Exercise Updated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+
+   });
+
 
    module.exports = router;
