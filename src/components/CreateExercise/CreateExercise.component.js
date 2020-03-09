eb4bbf2261840db190b9e5df8c5c4e978689bf15
Steve Archuleta <stevearchuleta@me.com>
2020-03-09 15:18:31 -0700

    write a constructor function inside the CreateExercise.js file that takes props, calls a super(props) -- becasue the constructor is being writting inside a JavaScript class-based component -- then code this.state to equal a JS object that defines the properties which are inside my MongoDB database, username:, description:, duration:, date: new Date(); add users: property to initial state in CreateExercises component and set it equal to and empty array; write four onChange methods in Create Exercise component: onChangeUsername, onChangeDescription, onChangeDuration, onChangeDate; write an onSubmit method in CreateExercise component and after event.preventingDefault, set an exercise object such that each aformentioned properties equal this.state.(name of aformentioned property); console.log exercises; ensure that this keyword references the CreateExercise class; write a componentDidMount method and hardcode a 'test user' username (for testing purposes) onto the user property array

--- src/components/CreateExercise/CreateExercise.component.js
+++ src/components/CreateExercise/CreateExercise.component.js
@@ -1,6 +1,71 @@
 import React, { Component } from 'react';
 
 class CreateExercise extends Component {
+   
+   constructor(props) {
+      //ensures that this refers to the CreateExercise class
+      this.onChangeUsername = this.onChangeUsername.bind(this);
+      this.onChangeDescription = this.onChangeDescription.bind(this);
+      this.onChangeDuration = this.onChangeDuration.bind(this);
+      this.onChangeDate = this.onChangeDate.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+
+      super(props);
+      this.state = {
+         username: '',
+         description: '',
+         duration: 0,
+         date: new Date(),
+         users: []
+      }
+   }
+   
+   //react lifecylce method that is automatically called just before anything is loaded/rendered onto the page
+   componentDidMount() {
+      this.setState({
+         users: ['test user'],
+         username: 'test user'
+      })
+   }
+
+   onChangeUsername(event) {
+      this.setState({
+         username: event.target.value
+      });
+   }
+
+   onChangeDescription(event) {
+      this.setState({
+         description: event.target.value
+      });
+   }
+
+   onChangeDuration(event) {
+      this.setState({
+         duration: event.target.value
+      });
+   }
+
+   onChangeDate(date) {
+      this.setState({
+         date: date
+      });
+   }
+
+   onSubmit(event) {
+      event.preventDefault();
+      const exercise = {
+         username: this.state.username,
+         description: this.state.description,
+         duration: this.state.duration,
+         date: this.state.date
+      }
+      console.log('[onSubmit: ]', exercise);
+
+      // now take user back to the list of exercises
+      window.location = '/';
+   }
+
    render() {
       return (
          <div>
