4e84b0a8d74d685f1430fcb82bd7bb7c79f2e795
Steve Archuleta <stevearchuleta@me.com>
2020-03-11 10:13:10 -0700

    connect front end to back end via axios.post request inside CreteUser component

--- src/components/CreateUser/CreateUser.component.js
+++ src/components/CreateUser/CreateUser.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateUser extends Component {
    constructor(props) {
@@ -27,6 +28,9 @@ class CreateUser extends Component {
       }
 
       console.log(user);
+
+      axios.post('http://localhost:5000/users/add', user)
+         .then(res => console.log(res.data));
 
       // set username back to a blank field
       this.setState({
